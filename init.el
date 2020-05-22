a4a5b59e03b7d9002c19c03e4f67aae12be82ca5
leejiahong <jia_hong@live.com.my>
2020-05-23 07:32:17 +0800

    Reduce lines from init.el by moving quotes to config.el

--- init.el
+++ init.el
@@ -1,46 +1,7 @@
 ;;; init.el --- Where all the magic begins. -*- lexical-binding: t -*-
 
 ;;; COMMENTARY:
-;;----------------------------------------------------------------------------
-;;    ___ _ __ ___   __ _  ___ ___
-;;   / _ \ '_ ` _ \ / _` |/ __/ __|
-;;  |  __/ | | | | | (_| | (__\__ \
-;; (_)___|_| |_| |_|\__,_|\___|___/
-;;
-;;  Citations
-;;
-;;     "Show me your ~/.emacs and I will tell you who you are."
-;;                                                         [Bogdan Maryniuk]
-;;
-;;     "Emacs is like a laser guided missile. It only has to be slightly
-;;      mis-configured to ruin your whole day."
-;;                                                            [Sean McGrath]
-;;
-;;     "While any text editor can save your files, only Emacs can save your
-;;      soul."
-;;                                                          [Per Abrahamsen]
-;;
-;; Source: http://www.mygooglest.com/fni/dot-emacs.html
-;;----------------------------------------------------------------------------
-
-;;----------------------------------------------------------------------------
-;;
-;;                __   _,--="=--,_   __
-;;               /  \."    .-.    "./  \
-;;              /  ,/  _   : :   _  \/` \
-;;              \  `| /o\  :_:  /o\ |\__/
-;;               `-'| :="~` _ `~"=: |
-;;                  \`     (_)     `/
-;;           .-"-.   \      |      /   .-"-.
-;;      .---{     }--|  /,.-'-.,\  |--{     }---.
-;;       )  (_)_)_)  \_/`~-===-~`\_/  (_(_(_)  (
-;;      (                                       )
-;;       )          Recreating Purcell         (
-;;      (            Emacs Experience           )
-;;       )                                     (
-;;      '---------------------------------------'
-;;
-;;----------------------------------------------------------------------------
+;; Read config.org for literate config.
 
 ;;; CODE:
 ;; SOURCE: https://github.com/jamiecollinson/dotfiles/blob/master/.emacs
